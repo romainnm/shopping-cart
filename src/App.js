e1e6af1a64e391fb86f79ee4bac53c0df3b93fbc
Romain N <insc.romain@gmail.com>
2021-10-05 22:02:21 -0500

    display list of product

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
+import React, { useState } from 'react';
+import items from './data'
 import './App.css';
 
+
 function App() {
+  const [products, setProducts] = useState(items);
+  console.log(products)
   return (
-    <div className="App">
-        App Component
+    <div>
+      <header>
+        <div className="navigation">
+          <h1>Happy pie</h1>
+          <nav>
+            <ul>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">Shop</a></li>
+            </ul>
+          </nav>
+        </div>
+        
+        <div className="shopping-cart">
+          <p>shopping cart (0)</p>
+        </div>
+      </header>
+      
+      <section className="shop-container">
+        {products.map((product, index) => {
+          const {id, name, price, img} = product;
+          return(
+            <article key={id} className="product">
+              <img src={img} alt="pie"/>
+              <h4 className="product-title">{name}</h4>
+              <p className="product-price">${price}</p>
+              <button>Add to cart</button>
+            </article>
+          )
+        })}
+      </section>
+      
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,51 @@
+const pies = [
+    {
+        id: 1,
+        name: 'cherry Pie',
+        price: 11,
+        img: './images/cherry.jpeg'
+    },
+    {
+        id: 2,
+        name: 'Apple Pie',
+        price: 15,
+        img: './images/apple.jpeg'
+    },
+    {
+        id: 3,
+        name: 'Pumpkin Pie',
+        price: 18,
+        img: './images/pumpkin.jpeg',
+    },
+    {
+        id: 4,
+        name: 'Nut Pie',
+        price: 10,
+        img: './images/nut.jpeg',
+    },
+    {
+        id: 5,
+        name: 'Lime Pie',
+        price: 13,
+        img: './images/lime.jpeg',
+    },
+    {
+        id: 6,
+        name: 'Banana Pie',
+        price: 6,
+        img: './images/banana.jpeg',
+    },
+    {
+        id: 7,
+        name: 'Raspberry Pie',
+        price: 20,
+        img: './images/raspberry.jpeg',
+    },
+    {
+        id: 8,
+        name: 'Kiwi Pie',
+        price: 5,
+        img: './images/kiwi.jpeg',
+    },
+];
+export default pies;
